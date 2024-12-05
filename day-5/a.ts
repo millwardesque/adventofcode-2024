95ddea229537c78a15667e9c22fb41c5121fb1b7
Chris Millward <cmillward@hey.com>
2024-12-05 09:11:06 -0500

    Day 5a: Cleanup

--- day-5/a.ts
+++ day-5/a.ts
@@ -12,51 +12,53 @@ export async function day5a(data: string[]) {
   const updateStrings = data.slice(endOfRuleIndex + 1);
   const updates = updateStrings.map((update) => update.split(','));
 
-  const rulesMap = rulesList.reduce((map, rule) => {
-    const [target, ruleCondition] = rule.split(RULE_SEPARATOR);
-
-    const targetRules = map.get(target) ?? new Set<string>();
-
-    targetRules.add(ruleCondition);
-    map.set(target, targetRules);
-    return map;
-  }, new Map<string, Set<string>>());
+  const rulesMap = rulesList.reduce(
+    addRuleToMap,
+    new Map<string, Set<string>>()
+  );
 
   const validUpdates = updates.filter((update) => {
     return Array.from(update).every((target, targetIndex) => {
       const targetRules = rulesMap.get(target) ?? new Set<string>();
-      const allRulesMatch = Array.from(targetRules).every((rule) => {
-        const ruleIndex = update.indexOf(rule);
-        if (ruleIndex === -1) {
-          return true;
-        } else {
-          return ruleIndex > targetIndex;
-        }
-      });
+      const allRulesMatch = Array.from(targetRules).every((rule) =>
+        doesTargetObeyRule(update, rule, targetIndex)
+      );
 
       return allRulesMatch;
     });
   });
 
-  const sum = validUpdates.reduce((acc, update) => {
-    const middleIndex = (update.length - 1) / 2;
-    acc += Number(update[middleIndex]);
-    return acc;
-  }, 0);
-
-  /**
-   * x1. Split rules from updates
-   * x2. Hashmap page number to must-be-before
-   * 3. For each update:
-   *    3b. For each element:
-   *        3c. For each rule for that element:
-   *            3d. If rule is in the update before element, return false
-   *    3e. If we get to the end w/o returning false, return true
-   * 4. If update is valid, add middle element to sum
-   */
-
-  console.log(validUpdates);
+  const sum = validUpdates.reduce(sumMiddleOfUpdate, 0);
   return sum;
 }
 
 await runSolution(day5a);
+
+function addRuleToMap(map: Map<string, Set<string>>, rule: string) {
+  const [target, ruleCondition] = rule.split(RULE_SEPARATOR);
+
+  const targetRules = map.get(target) ?? new Set<string>();
+
+  targetRules.add(ruleCondition);
+  map.set(target, targetRules);
+  return map;
+}
+
+function doesTargetObeyRule(
+  update: Array<string>,
+  rule: string,
+  targetIndex: number
+): boolean {
+  const ruleIndex = update.indexOf(rule);
+  if (ruleIndex === -1) {
+    return true;
+  } else {
+    return ruleIndex > targetIndex;
+  }
+}
+
+function sumMiddleOfUpdate(sum: number, update: Array<string>): number {
+  const middleIndex = (update.length - 1) / 2;
+  sum += Number(update[middleIndex]);
+  return sum;
+}
